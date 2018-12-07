510c571eb384baaa69b022f7bf9ab39a3c78ed72
Johannes Roeder <johannes.roeder@uni-bremen.de>
2018-12-07 16:11:48 +0100

    change csv reader

--- Q100_0.2_Demo.py
+++ Q100_0.2_Demo.py
@@ -22,7 +22,9 @@ date_time_index = pd.date_range('1/1/2012',
 energysystem = solph.EnergySystem(timeindex=date_time_index)
 
 # Read Data-file
-data = pd.read_csv("test-data_normiert.csv", sep=";")
+#data = pd.read_csv("test-data_normiert.csv", sep=";")
+data = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                                'test-data_normiert.csv'), sep=";")
 
 # Getting the interest rate used for all invest calculations
 rate = cfg.get('general_data', 'interest_rate')
